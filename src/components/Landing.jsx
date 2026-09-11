a99ce929970e0b6e81ed17bad00e8c77d7522fda
Alexandretta <alxsalazar21@gmail.com>
2026-09-11 15:14:50 -0500

    Se crean estilos para landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,24 @@
+import './../styles/landing.css';
+import logo from './../assets/petisos_logo.png';
+// import patita from './../assets/pata.png';
+
 export default function LandingPage({ onNavigate }) {
   return (
     <div className="landing-container">
       <header className="landing-header">
       </header>
-
+      <img src={logo} alt="Petisos Logo" className="landing-logo" />
       <section
         className="hero card"
         style={{ textAlign: "center", marginTop: "40px", padding: "40px" }}
       >
         <h1>PETISOS</h1>
         <p style={{ color: "var(--text-muted, #64748b)", margin: "20px 0" }}>
+
           🐾El segundo hogar de tu mascota.🐾
+        {/* <a href="https://www.flaticon.es/iconos-gratis/pata" title="pata iconos">Pata iconos creados por Nikita Golubev - Flaticon</a> */}
         </p>
+        {/* <img src={patita} alt="Patita" className="patita-icon" /> */}
         <div style={{ display: "flex", gap: "15px", justifyContent: "center" }}>
           <button
             className="btn-primary"
