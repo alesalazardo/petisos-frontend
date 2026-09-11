417b6b474bf352be393654027efdaace9722aa49
Alexandretta <alxsalazar21@gmail.com>
2026-09-11 14:13:55 -0500

    Se agrega landing page y algunos estilos a esta

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,94 @@
-import { useState, useEffect } from 'react';
-import { api } from './api/apiService';
-import TutorForm from './components/TutorForm';
-import TutorList from './components/TutorList';
-import MascotaForm from './components/MascotaForm';
-import MascotaList from './components/MascotaList';
+// import { useState, useEffect } from 'react';
+// import { api } from './api/apiService';
+// import TutorForm from './components/TutorForm';
+// import TutorList from './components/TutorList';
+// import MascotaForm from './components/MascotaForm';
+// import MascotaList from './components/MascotaList';
+import { Landing } from './pages/Landing';
 
 export default function App() {
-  const [activeTab, setActiveTab] = useState('tutores');
-  const [tutores, setTutores] = useState([]);
-  const [mascotas, setMascotas] = useState([]);
+  // const [activeTab, setActiveTab] = useState('tutores');
+  // const [tutores, setTutores] = useState([]);
+  // const [mascotas, setMascotas] = useState([]);
   
-  const [tutorEdit, setTutorEdit] = useState(null);
-  const [mascotaEdit, setMascotaEdit] = useState(null);
-  const [error, setError] = useState(null);
+  // const [tutorEdit, setTutorEdit] = useState(null);
+  // const [mascotaEdit, setMascotaEdit] = useState(null);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    cargarDatos();
-  }, []);
+  // useEffect(() => {
+  //   cargarDatos();
+  // }, []);
 
-  const cargarDatos = async () => {
-    try {
-      setError(null);
-      const dataTutores = await api.getTutores();
-      const dataMascotas = await api.getMascotas();
-      setTutores(dataTutores);
-      setMascotas(dataMascotas);
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  // const cargarDatos = async () => {
+  //   try {
+  //     setError(null);
+  //     const dataTutores = await api.getTutores();
+  //     const dataMascotas = await api.getMascotas();
+  //     setTutores(dataTutores);
+  //     setMascotas(dataMascotas);
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
-  // --- MÉTODOS TUTOR ---
-  const handleSaveTutor = async (data) => {
-    try {
-      setError(null);
-      if (tutorEdit) {
-        await api.updateTutor(tutorEdit.id, data);
-      } else {
-        await api.createTutor(data);
-      }
-      setTutorEdit(null);
-      cargarDatos();
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  // // --- MÉTODOS TUTOR ---
+  // const handleSaveTutor = async (data) => {
+  //   try {
+  //     setError(null);
+  //     if (tutorEdit) {
+  //       await api.updateTutor(tutorEdit.id, data);
+  //     } else {
+  //       await api.createTutor(data);
+  //     }
+  //     setTutorEdit(null);
+  //     cargarDatos();
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
-  const handleDeleteTutor = async (id) => {
-    if (!confirm('¿Eliminar tutor? Se borrarán sus mascotas asociadas en cascada.')) return;
-    try {
-      setError(null);
-      await api.deleteTutor(id);
-      cargarDatos();
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  // const handleDeleteTutor = async (id) => {
+  //   if (!confirm('¿Eliminar tutor? Se borrarán sus mascotas asociadas en cascada.')) return;
+  //   try {
+  //     setError(null);
+  //     await api.deleteTutor(id);
+  //     cargarDatos();
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
-  // --- MÉTODOS MASCOTA ---
-  const handleSaveMascota = async (data) => {
-    try {
-      setError(null);
-      if (mascotaEdit) {
-        await api.updateMascota(mascotaEdit.id, data);
-      } else {
-        await api.createMascota(data);
-      }
-      setMascotaEdit(null);
-      cargarDatos();
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  // // --- MÉTODOS MASCOTA ---
+  // const handleSaveMascota = async (data) => {
+  //   try {
+  //     setError(null);
+  //     if (mascotaEdit) {
+  //       await api.updateMascota(mascotaEdit.id, data);
+  //     } else {
+  //       await api.createMascota(data);
+  //     }
+  //     setMascotaEdit(null);
+  //     cargarDatos();
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
-  const handleDeleteMascota = async (id) => {
-    if (!confirm('¿Eliminar mascota?')) return;
-    try {
-      setError(null);
-      await api.deleteMascota(id);
-      cargarDatos();
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  // const handleDeleteMascota = async (id) => {
+  //   if (!confirm('¿Eliminar mascota?')) return;
+  //   try {
+  //     setError(null);
+  //     await api.deleteMascota(id);
+  //     cargarDatos();
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
   return (
     <div className="container">
-      <h1>🐾 Petisos - Panel de Control Veterinaria</h1>
+      <Landing />
+      {/* <h1>🐾 Petisos - Panel de Control Veterinaria</h1>
 
       {error && <div className="error-msg">⚠️ {error}</div>}
 
@@ -132,7 +134,7 @@ export default function App() {
             onDelete={handleDeleteMascota} 
           />
         </>
-      )}
+      )} */}
     </div>
   );
 }
